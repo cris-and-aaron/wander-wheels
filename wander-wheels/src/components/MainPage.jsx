fe0f14ffb256538c85bf7ed0fd60566e83251219
Aaron C <aaronc@MacBook-Pro-6.local>
2024-03-19 14:28:42 -0400

    map component made

--- wander-wheels/src/components/MainPage.jsx
+++ wander-wheels/src/components/MainPage.jsx
@@ -1,14 +1,19 @@
 import StartInput from "./StartInput";
 import EndInput from "./EndInput";
+import Map from "./Map";
 import { useState } from "react";
 const MainPage = () => {
-    const [startInput,setStartInput] = useState("");
-    const [endInput,setEndInput] = useState("");
-    return (
-    <div id="inputs">
-    <StartInput state={startInput}/>
-    <EndInput state={endInput}/>
-    </div>
-    );
-}
+  const [startInput, setStartInput] = useState("");
+  const [endInput, setEndInput] = useState("");
+
+  return (
+    <>
+      <div id="inputs">
+        <StartInput state={startInput} />
+        <EndInput state={endInput} />
+      </div>
+      <Map />
+    </>
+  );
+};
 export default MainPage;
