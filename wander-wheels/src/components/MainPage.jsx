141a8742c129d36d77db5ba81647af901121f19e
CrisM05 <Max42113@gmail.com>
2024-03-21 10:11:58 -0400

    The lord is upset with me and this map

--- wander-wheels/src/components/MainPage.jsx
+++ wander-wheels/src/components/MainPage.jsx
@@ -3,16 +3,23 @@ import EndInput from "./EndInput";
 import Map from "./Map";
 import { useState } from "react";
 import {handleFetch} from "../utils/utils.js";
-import API_KEY from "../utils/config.js"
+import testData from "../json/firstCall.json";
+import API_KEY from "../utils/config.js";
 
 const MainPage = () => {
     const [startInput,setStartInput] = useState("");
     const [endInput,setEndInput] = useState("");
+    const [route, setRoute] = useState(testData.route.legs[0].maneuvers.map(el => [el.startPoint.lat,el.startPoint.lng]));
     
     const handleSubmit = async (e) => {
       e.preventDefault();
       const {start,end} = Object.fromEntries(new FormData(e.target));
-      const data = await handleFetch(`https://www.mapquestapi.com/directions/v2/route?key=${API_KEY}&from=${start}&to=${end}`)
+      const [data,error] = await handleFetch(`https://www.mapquestapi.com/directions/v2/route?key=${API_KEY}&from=${start}&to=${end}`)
+      setRoute(data.route.legs[0].maneuvers.map(el => {
+        console.log(el.startPoint.lat, el.startPoint.lng);
+        return [el.startPoint.lat,el.startPoint.lng]
+
+      }));
       console.log(data);
       e.target.reset();
     };
@@ -26,7 +33,7 @@ const MainPage = () => {
           <button>Go</button>
         </form>
       </div>
-      <Map />
+      <Map route={route}/>
       </>
     );
 }
--- wander-wheels/src/components/Map.jsx
+++ wander-wheels/src/components/Map.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect } from "react";
-import API_KEY from "../config";
+import API_KEY from "../utils/config.js";
 
-export default function Map() {
+export default function Map({ route }) {
   useEffect(() => {
-    const success = (position) => {
-      const lat = position.coords.latitude;
-      const long = position.coords.longitude; // Fix typo: position.coords.longitude instead of latitude
-      const map = L.mapquest.map("map", {
-        center: [lat, long],
-        layers: L.mapquest.tileLayer("map"),
-        zoom: 12,
-      });
-    };
-
-    const error = () => {};
-
-    navigator.geolocation.getCurrentPosition(success, error, {
-      enableHighAccuracy: true,
-    });
-
+    const makeMap = async () => {
+      try {
     L.mapquest.key = API_KEY;
-  }, []);
+    L.mapquest.map("map", {
+      center: [17, -13],
+      layers: L.mapquest.tileLayer("map"),
+      zoom: 12,
+      zoomControl: false,
+    });
+    console.log(route);
+    var directions = L.mapquest.directions();
+    directions.route({
+      locations: route,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  }
+  makeMap();
+  },[route]);
+
   return <div id="map" style={{ width: "100%", height: "400px" }}></div>;
 }
